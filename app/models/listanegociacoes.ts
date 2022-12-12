053bbfaa05c67c8f85aed8d674610e8dc6de8a29
JpAmori <118912933+JpAmori@users.noreply.github.com>
2022-12-12 17:53:16 -0300

    Melhoria

--- app/models/listanegociacoes.ts
+++ app/models/listanegociacoes.ts
@@ -2,5 +2,18 @@ import Negociacao from "./negociacao.js";
 
 export class Negociacoes {
     private negociacoes: Array<Negociacao> = [];
-}
 
+    Add (negociacao: Negociacao){
+        this.negociacoes.push(negociacao);
+    }
+    list (): Array<Negociacao> {
+        return this.negociacoes;
+    }
+}
+//  Metodos Para adicionar Negociações
+/*const negociacoes = new Negociacoes();
+negociacoes.lista().array.forEach(n => {
+    n.
+});*/
+/*const negociacoes = new Negociacoes();
+negociacoes.methodAdd(negociacoes)*/
--- dist/js/models/listanegociacoes.js
+++ dist/js/models/listanegociacoes.js
@@ -2,4 +2,17 @@ export class Negociacoes {
     constructor() {
         this.negociacoes = [];
     }
+    Add(negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+    list() {
+        return this.negociacoes;
+    }
 }
+//  Metodos Para adicionar Negociações
+/*const negociacoes = new Negociacoes();
+negociacoes.lista().array.forEach(n => {
+    n.
+});*/
+/*const negociacoes = new Negociacoes();
+negociacoes.methodAdd(negociacoes)*/ 
